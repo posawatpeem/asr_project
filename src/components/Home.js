daa3ab25daa57dc37c53d910100c88b07e024e99
pkorn <pkorn003@gmail.com>
2021-05-06 06:47:00 +0700

    Add player handle func

--- src/components/Home.js
+++ src/components/Home.js
@@ -17,9 +17,37 @@ export default function Home() {
     
     const classes = useStyles();
     const [currentQueue, setCurrentQueue] = useState([]);
-   
+    const [shuffle, setShuffle] = useState(false);
+
     useEffect(() => {
         //setCurrentQueue(songs) songs for every page
+        // if (currentQueue.length === 0) {
+        //     spotifyApi.searchTracks("love").then(res => {
+        //         setCurrentQueue(
+        //             res.body.tracks.items.map(track => {
+        //               const smallestAlbumImage = track.album.images.reduce(
+        //                 (smallest, image) => {
+        //                   if (image.height < smallest.height) return image
+        //                   return smallest
+        //                 },
+        //                 track.album.images[0]
+        //               )
+            
+        //               return {
+        //                 artist: track.artists[0].name,
+        //                 title: track.name,
+        //                 uri: track.uri,
+        //                 albumUrl: smallestAlbumImage.url,
+        //               }
+        //             })
+        //           );
+        //     spotifyApi.getMyDevices
+        //     ().then(res => {
+        //         console.log(res.body);
+        //     })
+        //     });
+        // }
+        
     }, currentQueue.length);
 
     function chooseTrack(selectedTrack) {
@@ -33,14 +61,54 @@ export default function Home() {
         setCurrentQueue(currentQueue.slice(index,currentQueue.length));
         //songs =  currentQueue    update songs for every page
     }
+
+    function handlePause() {
+        spotifyApi.pause();
+    }
+
+    function handlePlay() {
+        spotifyApi.play();
+    }
+
+    function handleNext() {
+        spotifyApi.skipToNext();
+    }
+
+    function handlePrevious() {
+        spotifyApi.skipToPrevious();
+    }
+    
+    function handleRepeat() {
+        spotifyApi.setRepeat("context");
+    }
+    
+    function handleShuffle() {
+        spotifyApi.setShuffle(!shuffle);
+        setShuffle(!shuffle);
+    }
+
+    function handleSeek(){
+        spotifyApi.seek(30000);
+    }
+        
+        
+        
     
     return (
         <Layout style={{height:"100vh"}}>
-             <Header className={classes.header}>Player</Header>
+             {/* <Header className={classes.header} onClick={handlePause}>Stop</Header>
+             <Header className={classes.header} onClick={handlePlay}>Play</Header>
+             <Header className={classes.header} onClick={handleNext}>Next</Header>
+             <Header className={classes.header} onClick={handlePrevious}>Prev</Header>
+             <Header className={classes.header} onClick={handleRepeat}>re</Header>
+             <Header className={classes.header} onClick={handleShuffle}>shuffle</Header>
+             <Header className={classes.header} onClick={handleSeek}>to30</Header> */}
+
              <Content style={{ margin: '24px 16px 0'}}> 
                 <div>
                     <Player trackUris={currentQueue.map(track => track.uri)} />
                 </div>
+                {/* <div>shuffle:{shuffle}</div> */}
                 <div>
                     {currentQueue.map(track => (
                         <SongQueue
